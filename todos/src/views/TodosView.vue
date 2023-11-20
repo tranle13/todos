161b154a3cccd862903de79d7dbf53a79f5af46e
Sunny Le <letutran98@gmail.com>
2023-11-19 20:42:07 -0500

    Display added todos

--- todos/src/views/TodosView.vue
+++ todos/src/views/TodosView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import TodoCreator from '@/components/TodoCreator.vue';
+import TodoItem from '@/components/TodoItem.vue';
 import { uid } from 'uid';
 import { ref } from 'vue';
 
-interface Todo {
+export interface Todo {
   id: string;
   todo: string;
   isCompleted: boolean | null;
@@ -25,6 +26,9 @@ const createTodo = (todo: string) => {
   <main>
     <h1>Create Todo</h1>
     <TodoCreator @create-todo="createTodo"/>
+    <ul class="todo-list">
+      <TodoItem v-for="todo in todoList" :key="todo.id" :todo="todo"/>
+    </ul>
   </main>
 </template>
 
@@ -40,6 +44,22 @@ main {
   h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
   }
 }
 </style>
